782cd661dcc36ad9a2c906183453d428b541bcf0
Ashutosh <167632927+Ashu109@users.noreply.github.com>
2026-08-08 20:36:58 +0530

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility helpers for logging and errors."""
-from .logger import get_logger
-from .errors import ApplicationError
-
-__all__ = ["get_logger", "ApplicationError"]
--- utils/errors.py
+++
@@ -1,7 +0,0 @@
-"""Application-specific exceptions."""
-from __future__ import annotations
-
-
-class ApplicationError(Exception):
-    """Base exception for application-level errors."""
-    pass
