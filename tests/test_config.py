849ef811e5615357525b84bc61608f38c1ccef8f
Ashutosh <167632927+Ashu109@users.noreply.github.com>
2026-08-08 20:36:47 +0530

    Delete tests directory

--- tests/test_config.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from config.settings import load_config
-
-
-def test_config_contains_app_name() -> None:
-    cfg = load_config()
-    assert cfg["app"]["name"] == "Forsight"
